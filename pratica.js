5cd89cbc026517969eb7a5dd4354ceb17c7c02c6
jhonatas <jhonatas.melo71@gmail.com>
2024-09-20 21:45:55 -0300

    commit inicial

---
+++ pratica.js
@@ -0,0 +1,26 @@
+var carro = [{modelo: 'audio A3', marca: 'audi', ano: 2020},
+    {modelo: 'BMW M4', marca: 'BMW', ano: 2022}
+]
+for (let caracteristica in carro){
+    console.log(carro[caracteristica]);
+}
+
+for (let c of carro){
+console.log(c.ano)
+
+
+}
+
+
+var c=1;
+while (c <= 10){
+console.log(c)
+c++;
+}
+
+var t = 1;
+
+do{
+    console.log(t)
+    t++
+} while(t<1)
